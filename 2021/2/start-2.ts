831c69fc2a91126321309fd1bc49ffbd894804f8
Alex Washtell <alex@kasaku.co.uk>
2022-12-03 19:52:06 +0000

    2021 - 2

---
+++ 2021/2/start-2.ts
@@ -0,0 +1,44 @@
+import fs from 'fs';
+import { EOL } from 'os';
+import path from 'path';
+
+//const file = './files/example.txt';
+const file = './files/input.txt';
+
+type Position = {
+  horizontal: number;
+  depth: number;
+  aim: number;
+};
+
+const start = async () => {
+  const content = fs.readFileSync(path.join(__dirname, file), 'utf8');
+
+  const position = content.split(EOL).reduce(
+    (position, move) => {
+      const [direction, deltaString] = move.split(' ');
+      const delta = parseInt(deltaString);
+
+      switch (direction) {
+        case 'forward':
+          position.horizontal += delta;
+          position.depth += position.aim * delta;
+          break;
+        case 'up':
+          position.aim -= delta;
+          break;
+        case 'down':
+          position.aim += delta;
+          break;
+      }
+
+      return position;
+    },
+    { horizontal: 0, depth: 0, aim: 0 } as Position
+  );
+
+  console.log(position);
+  console.log(position.horizontal * position.depth);
+};
+
+start();
---
+++ 2021/2/start.ts
@@ -0,0 +1,42 @@
+import fs from 'fs';
+import { EOL } from 'os';
+import path from 'path';
+
+//const file = './files/example.txt';
+const file = './files/input.txt';
+
+type Position = {
+  horizontal: number;
+  depth: number;
+};
+
+const start = async () => {
+  const content = fs.readFileSync(path.join(__dirname, file), 'utf8');
+
+  const position = content.split(EOL).reduce(
+    (position, move) => {
+      const [direction, deltaString] = move.split(' ');
+      const delta = parseInt(deltaString);
+
+      switch (direction) {
+        case 'forward':
+          position.horizontal += delta;
+          break;
+        case 'up':
+          position.depth -= delta;
+          break;
+        case 'down':
+          position.depth += delta;
+          break;
+      }
+
+      return position;
+    },
+    { horizontal: 0, depth: 0 }
+  );
+
+  console.log(position);
+  console.log(position.horizontal * position.depth);
+};
+
+start();
