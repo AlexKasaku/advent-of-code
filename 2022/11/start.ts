af8c8c9791d2c8e19081ea96dcf903f42e04b063
Alex Washtell <alex@kasaku.co.uk>
2022-12-11 10:11:33 +0000

    2022 - 11 - Part 2 complete and bigint removed!

--- 2022/11/start.ts
+++ 2022/11/start.ts
@@ -1,22 +1,16 @@
 import { byDescending } from '@utils/sort';
 import fs from 'fs';
-import { EOL } from 'os';
 import path from 'path';
 
-const file = './files/example.txt';
-//const file = './files/input.txt';
+//const file = './files/example.txt';
+const file = './files/input.txt';
 
 const debugExpressions = false;
 
-type Item = {
-  value: bigint;
-  indexChain: number[];
-};
-
 type Monkey = {
-  items: Item[];
-  operation: (val: bigint) => bigint;
-  divisor: bigint;
+  items: number[];
+  operation: (val: number) => number;
+  divisor: number;
   trueMonkeyIndex: number;
   falseMonkeyIndex: number;
   inspections: number;
@@ -33,10 +27,8 @@ const monkeyInputRegex =
 
 const createOperation = (operation: string) => {
   // Uses eval, only to be used when we're sure of the input!
-  return (val: bigint): bigint => {
-    let expression = operation.replaceAll('old', val.toString() + 'n');
-
-    if (!expression.endsWith('n')) expression += 'n';
+  return (val: number): number => {
+    let expression = operation.replaceAll('old', val.toString());
 
     // Let's be safe, only allow digits, spaces, + and * through
     if (!operation.match(/[/d +*]+/))
@@ -56,11 +48,9 @@ const parseInput = (content: string): Monkey[] => {
 
     const { items, operation, divisor, trueMonkey, falseMonkey } = match.groups;
     monkeys.push({
-      items: items
-        .split(', ')
-        .map((x) => ({ value: BigInt(x), indexChain: [] })),
+      items: items.split(', ').map((x) => parseInt(x)),
       operation: createOperation(operation),
-      divisor: BigInt(divisor),
+      divisor: parseInt(divisor),
       trueMonkeyIndex: parseInt(trueMonkey),
       falseMonkeyIndex: parseInt(falseMonkey),
       inspections: 0,
@@ -73,43 +63,39 @@ const start = async () => {
   const content = fs.readFileSync(path.join(__dirname, file), 'utf8');
 
   const monkeys: Monkey[] = parseInput(content);
-  const rounds = 500;
+  const rounds = 10000;
+  const lowestCommonMultiple = monkeys
+    .map((x) => x.divisor)
+    .reduce((a, b) => a * b);
 
   for (let r = 0; r < rounds; r++) {
     for (const monkey of monkeys) {
       while (monkey.items.length) {
-        const item = monkey.items.shift()!;
+        let item = monkey.items.shift()!;
 
-        //const newValue = Math.floor(monkey.operation(item) / 3);
-        item.value = monkey.operation(item.value);
+        item = monkey.operation(item);
 
-        // if (newValue % 96577n == 0n) {
-        //   console.log(`Rounded ${newValue}`);
-        //   newValue /= 96577n;
-        // }
-
-        // if (newValue % monkey.divisor === 0n) {
-        //   newValue = newValue / monkey.divisor;
-        // }
+        // Reduce the item value down in a way that won't affect any
+        // further calculations by taking the remainder from dividing by
+        // the lowest common multiple
+        if (item > lowestCommonMultiple) {
+          item = item % lowestCommonMultiple;
+        }
 
         const newIndex =
-          item.value % monkey.divisor === 0n
+          item % monkey.divisor === 0
             ? monkey.trueMonkeyIndex
             : monkey.falseMonkeyIndex;
 
-        item.indexChain.push(newIndex);
         monkeys[newIndex].items.push(item);
 
         monkey.inspections += 1;
       }
     }
-    if (r % 100 === 0) console.log('Completed round ' + r);
+    //if (r % 250 === 0) console.log('Completed round ' + r);
   }
 
-  console.dir(monkeys[0].items[0].indexChain, {
-    maxArrayLength: null,
-    depth: null,
-  });
+  //console.dir(monkeys[0].items[0].indexChain, { depth: null });
 
   const topActive = monkeys.map((x) => x.inspections).sort(byDescending);
 
