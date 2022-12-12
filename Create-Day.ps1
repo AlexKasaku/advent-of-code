df5f9413e06da08bf975c29dc5272a21743b0429
Alex Washtell <alex@kasaku.co.uk>
2022-12-12 14:44:12 +0000

    Moved template and added script for running it

---
+++ Create-Day.ps1
@@ -0,0 +1,4 @@
+$year=$args[0]
+$day=$args[1]
+
+Copy-Item -r template $year\$day
---
+++ template/start.ts
@@ -0,0 +1,12 @@
+import fs from 'fs';
+import { EOL } from 'os';
+import path from 'path';
+
+const file = './files/example.txt';
+//const file = './files/input.txt';
+
+const start = async () => {
+  const content = fs.readFileSync(path.join(__dirname, file), 'utf8');
+};
+
+start();
