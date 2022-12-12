fe5631c4a9db70a6d87aa2153d846f81924c4458
Alex Washtell <alex@kasaku.co.uk>
2022-12-12 22:55:45 +0000

    2021 - 9 - Part 2

--- 2021/9/start.ts
+++ 2021/9/start.ts
@@ -1,4 +1,5 @@
 import { Grid } from '@utils/grid';
+import { byDescending } from '@utils/sort';
 import toSum from '@utils/toSum';
 import fs from 'fs';
 import { EOL } from 'os';
@@ -12,6 +13,7 @@ type Location = {
   y: number;
   height: number;
   lowPoint: boolean;
+  basin?: number;
 };
 
 const start = async () => {
@@ -19,7 +21,7 @@ const start = async () => {
 
   const contentSplit = content.split(EOL).map((l) => l.split(''));
 
-  const grid = new Grid(
+  const grid = new Grid<Location>(
     contentSplit[0].length,
     contentSplit.length,
     ({ x, y }) => ({
@@ -30,19 +32,57 @@ const start = async () => {
     })
   );
 
-  // Find all low points
+  // Find all low points and assign them a basin
+  let basin = 0;
   for (const point of grid.Values.flat()) {
-    if (grid.getNeighbours(point).every((p) => point.height < p.height))
+    if (grid.getNeighbours(point).every((p) => point.height < p.height)) {
       point.lowPoint = true;
+      point.basin = basin++;
+    }
   }
 
-  // Get total risk level
-  const totalRiskLevel = grid.Values.flat()
-    .filter((x) => x.lowPoint)
-    .map((x) => x.height + 1)
-    .reduce(toSum);
+  // Find all points belonging to basins
+  for (let x = 0; x < basin; x++) {
+    let toVisit = [grid.Values.flat().find((p) => p.basin === x)];
 
-  console.log(totalRiskLevel);
+    // Keep going until visit array is empty
+    while (toVisit.length > 0) {
+      const candidate = toVisit.shift()!;
+
+      candidate.basin = x;
+
+      // Add any neighbours that are under 9 in height and not yet in a basin
+      toVisit.push(
+        ...grid
+          .getNeighbours(candidate)
+          .filter((x) => x.height < 9 && x.basin === undefined)
+      );
+    }
+  }
+
+  // Now count the largest basins
+  const isDefined = <T>(item: T | undefined): item is T => item !== undefined;
+  const basins = grid.Values.flat()
+    .map((x) => x.basin)
+    .filter(isDefined)
+    .reduce((a, b) => {
+      a[b] = (a[b] ?? 0) + 1;
+      return a;
+    }, [] as number[]);
+
+  console.log(
+    basins
+      .sort(byDescending)
+      .slice(0, 3)
+      .reduce((a, b) => a * b)
+  );
+  // Get total risk level
+  // const totalRiskLevel = grid.Values.flat()
+  //   .filter((x) => x.lowPoint)
+  //   .map((x) => x.height + 1)
+  //   .reduce(toSum);
+
+  // console.log(totalRiskLevel);
 };
 
 start();
