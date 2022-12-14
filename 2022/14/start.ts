ac9c862f585c9269e2d0fdbd3caf2d13cef5cac0
Alex Washtell <alex@kasaku.co.uk>
2022-12-14 14:46:00 +0000

    2022 - 14 - More performance squeezed out

--- 2022/14/start.ts
+++ 2022/14/start.ts
@@ -18,10 +18,8 @@ const start = async () => {
 
   const grid = createGridFromPoints(points, true);
 
-  type Sand = Point & { spawnedFrom: Point };
-  const sandsInMotion: Sand[] = [
-    { x: 500, y: 0, spawnedFrom: { x: 500, y: 0 } },
-  ];
+  type Sand = Point;
+  const sandInMotion: Sand = { x: 500, y: 0 };
 
   // Using cycles for infinite-loop protection, if this runs out before puzzle is done
   // then likely there's a problem.
@@ -36,61 +34,47 @@ const start = async () => {
     //renderGrid(grid);
 
     // For each sand in motion
-    for (const sandInMotion of sandsInMotion) {
-      const nextPoint = getNextPointForSand(grid, sandInMotion);
+    const nextPoint = getNextPointForSand(grid, sandInMotion);
 
-      switch (nextPoint) {
-        case 'off':
-          // Finished!
+    switch (nextPoint) {
+      case 'off':
+        // Finished!
+        //renderGrid(grid);
+        console.log(
+          `Sand off grid! Total sand at rest: ${sandAtRest} (took ${cycle} cycles)`
+        );
+        console.log(
+          `Execution took ${performance.now() - startTime} milliseconds`
+        );
+        return;
+
+      case 'rest':
+        // Store this sand in the grid
+        sandAtRest++;
+        grid.get(sandInMotion)!.content = 'sand';
+
+        // Part 2: Is this point the sand spawn? If so then done!
+        if (sandInMotion.x === 500 && sandInMotion.y === 0) {
           //renderGrid(grid);
           console.log(
-            `Sand off grid! Total sand at rest: ${sandAtRest} (took ${cycle} cycles)`
+            `Sand reached spawn point! Total sand at rest: ${sandAtRest} (took ${cycle} cycles)`
           );
           console.log(
             `Execution took ${performance.now() - startTime} milliseconds`
           );
+
           return;
-
-        case 'rest':
-          // Store this sand in the grid
-          sandAtRest++;
-          grid.set(sandInMotion, { ...sandInMotion, content: 'sand' });
-
-          // Part 2: Is this point the sand spawn? If so then done!
-          if (
-            sandInMotion.x === sandInMotion.spawnedFrom.x &&
-            sandInMotion.y === sandInMotion.spawnedFrom.y
-          ) {
-            //renderGrid(grid);
-            console.log(
-              `Sand reached spawn point! Total sand at rest: ${sandAtRest} (took ${cycle} cycles)`
-            );
-            console.log(
-              `Execution took ${performance.now() - startTime} milliseconds`
-            );
-
-            return;
-          }
-
-          // Remove this sand in motion
-          findAndRemove(
-            sandsInMotion,
-            (val) => val.x == sandInMotion.x && val.y == sandInMotion.y
-          );
-
-          // Spawn next sand
-          sandsInMotion.push({
-            x: sandInMotion.spawnedFrom.x,
-            y: sandInMotion.spawnedFrom.y,
-            spawnedFrom: sandInMotion.spawnedFrom,
-          });
-          break;
-
-        default: {
-          // Sand continues to move, update it's position
-          sandInMotion.x = nextPoint.x;
-          sandInMotion.y = nextPoint.y;
         }
+
+        // Replace this sand in motion back to spawn
+        sandInMotion.x = 500;
+        sandInMotion.y = 0;
+        break;
+
+      default: {
+        // Sand continues to move, update it's position
+        sandInMotion.x = nextPoint.x;
+        sandInMotion.y = nextPoint.y;
       }
     }
   }
