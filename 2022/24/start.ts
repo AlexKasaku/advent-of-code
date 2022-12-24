534958dac668fe6ea12cbc5c47a332cca704633d
Alex Washtell <alex@kasaku.co.uk>
2022-12-24 09:12:46 +0000

    2022 - 24 - Precalculating all states

--- 2022/24/start.ts
+++ 2022/24/start.ts
@@ -1,27 +1,34 @@
+import range from '@utils/range';
 import fs from 'fs';
 import path from 'path';
-import { buildGridFromState, parseInput, renderGrid } from './utils';
+import {
+  buildAllGridStates,
+  buildGridFromState,
+  getBlizzardsAtStep,
+  parseInput,
+  renderGrid,
+} from './utils';
 
 //const file = './files/example.txt';
-//const file = './files/example.2.txt';
-const file = './files/input.txt';
+const file = './files/example.2.txt';
+//const file = './files/input.txt';
+//const file = './files/test.txt';
 
 const start = async () => {
   const content = fs.readFileSync(path.join(__dirname, file), 'utf8');
 
   const setup = parseInput(content);
 
-  console.log(setup);
+  console.log(setup.width);
+  console.log(setup.height);
 
-  const grid = buildGridFromState(
-    setup.width,
-    setup.height,
-    setup.start,
-    setup.end,
-    setup.blizzards
-  );
+  const uniqueStates = (setup.width - 2) * (setup.height - 2);
+  console.log(uniqueStates);
 
-  renderGrid(grid);
+  // Blizzard states loop, so precalculate all states!
+  const gridStates = buildAllGridStates(setup, uniqueStates);
+
+  // Now, let's solve it (eep).
 };
 
 start();
--- 2022/24/utils.ts
+++ 2022/24/utils.ts
@@ -1,4 +1,5 @@
 import { Grid, Position } from '@utils/grid';
+import range from '@utils/range';
 import { EOL } from 'os';
 import { Blizzard, InitialSetup, Space } from './types';
 
@@ -76,11 +77,76 @@ export const buildGridFromState = (
   grid.get(start)!.start = true;
   grid.get(end)!.end = true;
 
-  for (const blizzard of blizzards)
+  for (const blizzard of blizzards) {
+    //console.log(blizzard);
     grid.get(blizzard.current)!.blizzards.push(blizzard);
+  }
 
   return grid;
 };
+
+export const getBlizzardsAtStep = (
+  width: number,
+  height: number,
+  initialBlizzards: Blizzard[],
+  step: number
+): Blizzard[] => {
+  const playWidth = width - 2;
+  const playHeight = height - 2;
+  return initialBlizzards.map((blizzard) => {
+    switch (blizzard.direction) {
+      case 'R':
+        return {
+          ...blizzard,
+          current: {
+            x: 1 + ((blizzard.current.x - 1 + (step % playWidth)) % playWidth),
+            y: blizzard.current.y,
+          },
+        };
+      case 'L':
+        return {
+          ...blizzard,
+          current: {
+            x:
+              1 +
+              ((playWidth + blizzard.current.x - 1 - (step % playWidth)) %
+                playWidth),
+            y: blizzard.current.y,
+          },
+        };
+      case 'U':
+        return {
+          ...blizzard,
+          current: {
+            x: blizzard.current.x,
+            y:
+              1 +
+              ((playHeight + blizzard.current.y - 1 - (step % playHeight)) %
+                playHeight),
+          },
+        };
+      case 'D':
+        return {
+          ...blizzard,
+          current: {
+            x: blizzard.current.x,
+            y:
+              1 + ((blizzard.current.y - 1 + (step % playHeight)) % playHeight),
+          },
+        };
+    }
+  });
+};
+export const buildAllGridStates = (setup: InitialSetup, uniqueStates: number) =>
+  range(0, uniqueStates - 1).map((i) =>
+    buildGridFromState(
+      setup.width,
+      setup.height,
+      setup.start,
+      setup.end,
+      getBlizzardsAtStep(setup.width, setup.height, setup.blizzards, i)
+    )
+  );
 
 const getRenderChar = (space: Space): string => {
   if (space.start || space.end) return '.';
