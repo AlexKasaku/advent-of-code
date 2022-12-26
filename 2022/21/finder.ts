8d20935120bc962e4c659ff9d45992ee60a13baf
Alex Washtell <alex@kasaku.co.uk>
2022-12-26 18:15:53 +0000

    2022 - 21 - Parts with solver

---
+++ 2022/21/finder.ts
@@ -0,0 +1,24 @@
+const equation =
+  '(10 * (((131234706858508 - ((((((((924 + ((150 + (((2 * (((905 + (((((((((((((2 * ((((3 * (((((((854 + (((37 * ((((((341 + (((7 * (187 + ((28 + (((((((556 + ((((x - 332) * 36) + 685) / 5)) * 2) - 737) + 978) + 164) / 3) - 16)) / 3))) - 849) * 2)) + 748) / 7) - 349) / 4) + 793)) - 61) / 7)) * 8) - 983) / 7) + 974) * 2) - 531)) + 181) / 2) - 548)) + 750) / 2) - 657) / 11) + 85) * 28) + 877) + 709) / 3) - 430) * 2) + 839)) / 2) - 60)) - 635) * 2)) / 4)) * 2) - 478) / 2) - 818) / 3) + 39) * 7)) / 5) + 553))';
+const expected = 46779208742730;
+
+let lowerX = 0;
+let upperX = 46779208742730;
+
+let resultLower = 0;
+let resultUpper = 0;
+
+let i = 100;
+while (i--) {
+  console.log(`Testing: ${lowerX} < x < ${upperX} (${upperX - lowerX})`);
+
+  const testX = Math.floor((lowerX + upperX) / 2);
+  const resultX = eval(equation.replace('x', testX.toString()));
+
+  if (resultX == expected) {
+    console.log('X = ' + testX);
+    break;
+  }
+  if (resultX < expected) upperX = testX;
+  else lowerX = testX;
+}
--- 2022/21/start.ts
+++ 2022/21/start.ts
@@ -3,16 +3,33 @@ import path from 'path';
 import { parseInput } from './utils';
 
 //const file = './files/example.txt';
-const file = './files/input.txt';
+//const file = './files/example.2.txt';
+//const file = './files/input.txt';
+const file = './files/input.2.txt';
+
+const getReverseOperator = (op: string) => {
+  switch (op) {
+    case '*':
+      return '/';
+    case '/':
+      return '*';
+    case '+':
+      return '-';
+    case '-':
+      return '+';
+  }
+  return '=';
+};
 
 const start = async () => {
   const content = fs.readFileSync(path.join(__dirname, file), 'utf8');
 
   const monkeys = parseInput(content);
 
+  // Brute force to find the real value
   const traverseStack = [monkeys.get('root')];
   const processStack = [];
-  const monkeyValues = new Map<string, number>();
+  const monkeyValues = new Map<string, number | boolean | string>();
 
   while (traverseStack.length > 0) {
     const node = traverseStack.pop()!;
@@ -28,27 +45,77 @@ const start = async () => {
   }
 
   // Now process the monkeys
-  let total = 0;
+
+  // Not between 0 and 100000
+
+  const solverOperations = [];
+
   while (processStack.length > 0) {
     const node = processStack.pop()!;
 
-    const leftValue = monkeyValues.get(node.left!);
+    const leftValue = node.left == 'humn' ? 'x' : monkeyValues.get(node.left!);
 
     if (leftValue === undefined) throw 'Could not get left value!';
-    const rightValue = monkeyValues.get(node.right!);
+    const rightValue =
+      node.right == 'humn' ? 'x' : monkeyValues.get(node.right!);
 
     if (rightValue === undefined) throw 'Could not get right value!';
 
     // Calculate
-    const toEval = `${leftValue} ${node.operation} ${rightValue}`;
+    const toEval = `${leftValue} ${node.operation} ${rightValue}`.replace(
+      '=',
+      '=='
+    );
 
-    const value = eval(toEval);
+    if (toEval.indexOf('x') > -1) {
+      // Contains unknown value, push as string
+      monkeyValues.set(node.name, `(${toEval})`);
 
-    monkeyValues.set(node.name, value);
+      // Push the other operand to the solvers
+      if (typeof leftValue === 'string') {
+        // Left contains x
+        solverOperations.push(
+          `x ${getReverseOperator(node.operation!)} ${rightValue}`
+        );
+      } else {
+        // Right contains x - requires more finesse
+        switch (node.operation!) {
+          case '*':
+            solverOperations.push(`x / ${leftValue}`);
+            break;
+          case '/':
+            solverOperations.push(`${leftValue} / x `);
+            break;
+          case '+':
+            solverOperations.push(`x - ${leftValue}`);
+            break;
+          case '-':
+            solverOperations.push(`${leftValue} - x`);
+            break;
+        }
+      }
+    } else {
+      // We can work this out now, add the value
+      const value = eval(toEval);
+
+      monkeyValues.set(node.name, value);
+    }
   }
 
-  console.log(monkeyValues);
-  console.log(monkeyValues.get('root'));
+  // Write out what is at root
+  const equation = monkeyValues.get('root') as string;
+  console.log(equation);
+
+  // Walk backwards through equation
+  let finalValue = solverOperations.pop()?.split(' = ')[1];
+
+  while (solverOperations.length > 0) {
+    finalValue = eval(
+      solverOperations.pop()!.replace('x', finalValue!.toString())
+    );
+  }
+
+  console.log(`x = ${finalValue}`);
 };
 
 start();
