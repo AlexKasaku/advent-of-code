33e8816d08aa1d57b855523c516bc19d364ec853
Alex Washtell <alex@kasaku.co.uk>
2023-11-29 11:30:47 +0000

    Prep for 2023. From this point, 2022 code may break as not looking to maintain backwards compatibility.

--- 2022/1/1-alternate.ts
+++ 2022/1/1-alternate.ts
@@ -1,20 +1,22 @@
-import fs from "fs";
-import { EOL } from "os";
-import path from "path";
+import { byDescending } from '@utils/sort';
+import toSum from '@utils/toSum';
+import fs from 'fs';
+import { EOL } from 'os';
+import path from 'path';
 
 const start = async () => {
-  const content = fs.readFileSync(path.join(__dirname, "1.input.txt"), "utf8");
+  const content = fs.readFileSync(path.join(__dirname, '1.input.txt'), 'utf8');
 
   const elves = content.split(EOL + EOL);
   const totals = elves.map((elf) =>
     elf
       .split(EOL)
       .map((x) => parseInt(x))
-      .reduce((x, y) => x + y)
+      .reduce(toSum)
   );
 
-  const topThree = totals.sort((n1, n2) => n2 - n1).slice(0, 3);
-  const combined = topThree.reduce((x, y) => x + y);
+  const topThree = totals.sort(byDescending).slice(0, 3);
+  const combined = topThree.reduce(toSum);
 
   console.log(topThree);
   console.log(combined);
---
+++ 2023/1/start.ts
@@ -0,0 +1,14 @@
+import fs from 'fs';
+import path from 'path';
+import { parseInput } from './utils';
+
+const file = './files/example.txt';
+//const file = './files/input.txt';
+
+const start = async () => {
+  const content = fs.readFileSync(path.join(__dirname, file), 'utf8');
+
+  const x = parseInput(content);
+};
+
+start();
