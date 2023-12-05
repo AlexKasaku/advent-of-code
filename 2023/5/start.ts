6d150aa8805c4ebe30c69b7fa86418fdac2c69cb
Alex Washtell <alex@kasaku.co.uk>
2023-12-05 06:13:10 +0000

    2023 - Day 5 - Part 2, naive implementation. Not sure if completes.

--- 2023/5/start.ts
+++ 2023/5/start.ts
@@ -2,12 +2,27 @@ import fs from 'fs';
 import path from 'path';
 import { parseInput } from './utils';
 import { Almanac } from './types';
+import range from '@utils/range';
+import { chunk } from 'lodash';
 
-const debugMode = true;
+const debugMode = false;
 const debug = (...params: any[]) => debugMode && console.log(...params);
 const log = (...params: any[]) => console.log(...params);
 
-const part1 = (almanac: Almanac) => {
+// const expandSeedRange = (seedRanges: number[]) => {
+//   const seeds = [];
+//   for (let index = 0; index < seedRanges.length; index += 2) {
+//     seeds.push(
+//       ...range(
+//         seedRanges[index],
+//         seedRanges[index] + seedRanges[index + 1] - 1,
+//       ),
+//     );
+//   }
+//   return seeds;
+// };
+
+const run = (almanac: Almanac) => {
   const maps = [
     almanac.seedSoil,
     almanac.soilFertilizer,
@@ -20,27 +35,39 @@ const part1 = (almanac: Almanac) => {
 
   let lowest = Number.MAX_SAFE_INTEGER;
 
-  for (const seed of almanac.seeds) {
-    debug('Seed ' + seed);
-    let destination = seed;
+  // Part 1
+  //const processedSeeds = almanac.seeds;
 
-    for (const map of maps) {
-      for (const itemMap of map) {
-        if (
-          destination >= itemMap.source &&
-          destination <= itemMap.source + itemMap.range - 1
-        ) {
-          const oldDest = destination;
-          const diff = destination - itemMap.source;
-          destination = itemMap.dest + diff;
+  // Part 2
+  const seedGroups = chunk(almanac.seeds, 2);
 
-          debug(`Mapped ${oldDest} => ${destination}`);
-          break;
+  for (const seedGroup of seedGroups) {
+    for (
+      let seed = seedGroup[0];
+      seed < seedGroup[0] + seedGroup[1] - 1;
+      seed++
+    ) {
+      debug('Seed ' + seed);
+      let destination = seed;
+
+      for (const map of maps) {
+        for (const itemMap of map) {
+          if (
+            destination >= itemMap.source &&
+            destination <= itemMap.source + itemMap.range - 1
+          ) {
+            const oldDest = destination;
+            const diff = destination - itemMap.source;
+            destination = itemMap.dest + diff;
+
+            debug(`Mapped ${oldDest} => ${destination}`);
+            break;
+          }
         }
       }
-    }
 
-    if (destination < lowest) lowest = destination;
+      if (destination < lowest) lowest = destination;
+    }
   }
 
   log('Lowest: ' + lowest);
@@ -51,9 +78,7 @@ const start = async (file: string) => {
 
   const almanac = parseInput(content);
 
-  //console.log(almanac);
-
-  part1(almanac);
+  run(almanac);
 };
 
 //start('./files/example.txt');
