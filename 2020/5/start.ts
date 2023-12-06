f150346579f8d32066abb1d056dc5566f77e4249
Alex Washtell <alex@kasaku.co.uk>
2023-12-06 14:27:57 +0000

    2020 - Day 5 - Part 2

--- 2020/5/start.ts
+++ 2020/5/start.ts
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import { parseInput } from './utils';
+import { createAndInitArray, createArray } from '@utils/createArray';
 
 const debugMode = true;
 const debug = (...params: any[]) => debugMode && console.log(...params);
@@ -11,8 +12,12 @@ const start = async (file: string) => {
 
   const seats = parseInput(content);
 
-  // Part 1
   let highest = 0;
+  const seatPlan = createAndInitArray(() => null, 128, 8) as (
+    | number
+    | null
+  )[][];
+
   for (const seat of seats) {
     const row = parseInt(
       seat.substring(0, 7).replaceAll('B', '1').replaceAll('F', '0'),
@@ -23,10 +28,19 @@ const start = async (file: string) => {
       2,
     );
     const seatId = row * 8 + column;
+
     debug(`${seat}: row ${row}, column ${column}, seat ID ${seatId}`);
+    seatPlan[row][column] = seatId;
 
     if (seatId > highest) highest = seatId;
   }
+
+  // Part 2, find rows with some emtpy seats:
+  const rowsWithSeats = seatPlan.filter(
+    (row) => !row.every((c) => c === null) && row.some((c) => c === null),
+  );
+
+  console.dir(rowsWithSeats, { depth: null });
 
   log(highest);
 };
