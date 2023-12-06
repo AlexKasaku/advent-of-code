4ac191e9e0046786f7da158c198875254bd7c213
Alex Washtell <alex@kasaku.co.uk>
2023-12-06 14:41:48 +0000

    2020 - Day 6 - Part 2

--- 2020/6/start.ts
+++ 2020/6/start.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import { parseInput } from './utils';
 
-const debugMode = true;
+const debugMode = false;
 const debug = (...params: any[]) => debugMode && console.log(...params);
 const log = (...params: any[]) => console.log(...params);
 
@@ -11,17 +11,26 @@ const start = async (file: string) => {
 
   const answerGroups = parseInput(content);
 
-  let total = 0;
+  let totalForAny = 0;
+  let totalForAll = 0;
 
   for (const group of answerGroups) {
-    const answerSet = new Set<string>();
-    for (const answer of group) for (const char of answer) answerSet.add(char);
+    const answerMap = new Map<string, number>();
+    for (const answer of group)
+      for (const char of answer) {
+        answerMap.set(char, (answerMap.get(char) ?? 0) + 1);
+      }
 
-    debug('Set size: ' + answerSet.size);
-    total += answerSet.size;
+    debug('Set size: ' + answerMap.size);
+
+    totalForAny += answerMap.size;
+    totalForAll += [...answerMap.values()].filter(
+      (x) => x == group.length,
+    ).length;
   }
 
-  log('Total: ' + total);
+  log('Total For Any: ' + totalForAny);
+  log('Total For All: ' + totalForAll);
 };
 
 //start('./files/example.txt');
