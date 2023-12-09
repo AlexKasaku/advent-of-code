074eda0d823ff8fda1736aea53ea8577ef6f1f94
Alex Washtell <alex@kasaku.co.uk>
2023-12-09 13:21:41 +0000

    Another test case for LCM using primes

--- utils/lowestCommonMultiple.test.ts
+++ utils/lowestCommonMultiple.test.ts
@@ -9,6 +9,7 @@ describe('lowestCommonMultiple', () => {
     [[6, 7], 42],
     [[6, 14], 42],
     [[6, 14, 3, 2, 7], 42],
+    [[2, 3, 5, 7, 11, 13], 30030],
   ])('for %p, returns %p', (numbers, expected) => {
     expect(lowestCommonMultiple(...numbers)).toEqual(expected);
   });
