9a6de0b1b8c5a9c023e6c482df63b8b2039294c9
Alex Washtell <alex@kasaku.co.uk>
2023-12-14 09:49:22 +0000

    2023 - Day 14 - Part 2

--- 2023/14/start.ts
+++ 2023/14/start.ts
@@ -2,8 +2,9 @@ import fs from 'fs';
 import path from 'path';
 import { parseInput, renderGrid, tiltGrid } from './utils';
 import toSum from '@utils/toSum';
+import { Direction } from '@utils/grid';
 
-const debugMode = false;
+const debugMode = true;
 const debug = (...params: any[]) => debugMode && console.log(...params);
 const log = (...params: any[]) => console.log(...params);
 
@@ -12,16 +13,61 @@ const start = async (file: string) => {
 
   const grid = parseInput(content);
 
-  if (debugMode) renderGrid(grid);
-  tiltGrid(grid);
-  if (debugMode) renderGrid(grid);
+  const spinCycle = ['N', 'W', 'S', 'E'];
+  let step = 0;
+  let cycle = 1;
+  const loads = [];
 
-  const totalLoad = grid.Values.flat()
-    .filter((r) => r.isRock && !r.isStaticRock)
-    .map((r) => grid.Height - r.y)
-    .reduce(toSum);
+  // Go along enough until we'll hit repeats
+  while (cycle < 200) {
+    const direction = spinCycle[step % spinCycle.length] as Direction;
+    tiltGrid(grid, direction);
 
-  log(totalLoad);
+    step++;
+    if (step % spinCycle.length == 0) {
+      // Finished a cycle
+      const totalLoad = grid.Values.flat()
+        .filter((r) => r.isRock && !r.isStaticRock)
+        .map((r) => grid.Height - r.y)
+        .reduce(toSum);
+
+      debug(`Step: ${step}. Cycle: ${cycle}. Load: ${totalLoad}`);
+
+      loads.push(totalLoad);
+      cycle++;
+    }
+  }
+
+  // Find the pattern to predict value at step 1000000000.
+  // Assume a pattern size of under 50 that is repeating after 100 cycles.
+  // It doesn't matter if this is double/treble the actual pattern as the maths is the same.
+  const lastLoads = loads.slice(-100);
+  let foundRepeatSize = 0;
+  for (let repeatSize = lastLoads.length / 2; repeatSize > 0; repeatSize--) {
+    let foundRepeat = true;
+    for (let i = 0; i <= repeatSize; i++) {
+      if (
+        lastLoads[lastLoads.length - 1 - i] !==
+        lastLoads[lastLoads.length - 1 - repeatSize - i]
+      ) {
+        foundRepeat = false;
+        break;
+      }
+    }
+    if (foundRepeat) {
+      debug(`Repeat pattern length = ${repeatSize}`);
+      foundRepeatSize = repeatSize;
+      break;
+    }
+  }
+
+  if (foundRepeatSize == 0) throw 'Could not identify repeating pattern';
+
+  const repeat = loads.slice(-foundRepeatSize);
+  debug(repeat);
+
+  const end = 1000000000 - cycle;
+  log(repeat[end % foundRepeatSize]);
 };
 
 //start('./files/example.txt');
--- 2023/14/utils.ts
+++ 2023/14/utils.ts
@@ -1,4 +1,4 @@
-import { Grid } from '@utils/grid';
+import { Direction, Grid } from '@utils/grid';
 import { EOL } from 'os';
 import { Space } from './types';
 
@@ -14,26 +14,58 @@ export const parseInput = (input: string): Grid<Space> => {
   }));
 };
 
-export const tiltGrid = (grid: Grid<Space>): void => {
-  const rollableRocks = grid.Values.flat().filter(
-    (s) => s.isRock && !s.isStaticRock,
-  );
+const getRocksInOrder = (grid: Grid<Space>, direction: Direction): Space[] => {
+  // To ensure no clashes we need to process the rocks in the right order.
+  let rocks: Space[] = [];
+
+  switch (direction) {
+    case 'N':
+    case 'W':
+      // Top-to-bottom, left-to-right
+      rocks = grid.Values.flat().filter((s) => s.isRock && !s.isStaticRock);
+      break;
+
+    case 'S':
+      // Bottom-to-top, left-to-right
+      for (let rowIndex = grid.Height - 1; rowIndex >= 0; rowIndex--) {
+        rocks.push(
+          ...grid.Values[rowIndex].filter((s) => s.isRock && !s.isStaticRock),
+        );
+      }
+      break;
+
+    case 'E':
+      // Top-to-bottom, right-to-left
+      for (const row of grid.Values)
+        rocks.push(
+          ...[...row].filter((s) => s.isRock && !s.isStaticRock).reverse(),
+        );
+      break;
+  }
+
+  return rocks;
+};
+
+export const tiltGrid = (grid: Grid<Space>, direction: Direction): void => {
+  const rollableRocks = getRocksInOrder(grid, direction);
 
   for (const rock of rollableRocks) {
-    // Look north to find first rock and move under that. As we're working top to bottom
-    // we know all the rocks above us have already moved. If the space is undefined, we're at the
-    // top already.
-    const firstBlockedSpace = grid
-      .getAllInDirection(rock, 'N')
-      .find((s) => s?.isRock);
-    const spaceUnderRock =
-      firstBlockedSpace === undefined
-        ? grid.get({ x: rock.x, y: 0 })
-        : grid.get({ x: firstBlockedSpace.x, y: firstBlockedSpace.y + 1 });
+    const aheadOfRock = grid.getAllInDirection(rock, direction);
+    const firstBlockedIndex = aheadOfRock.findIndex((s) => s?.isRock);
+
+    let destination: Space | undefined = undefined;
+    if (firstBlockedIndex > 0) {
+      destination = aheadOfRock[firstBlockedIndex - 1]!;
+    } else if (firstBlockedIndex == -1) {
+      // Move to edge
+      destination = aheadOfRock[aheadOfRock.length - 1]!;
+    } else {
+      // Index is 0, which means we're not moving as we're already blocked.
+    }
 
     // Swap the rock and space over, if it's actually moved!
-    if (spaceUnderRock !== rock) {
-      spaceUnderRock!.isRock = true;
+    if (destination) {
+      destination.isRock = true;
       rock.isRock = false;
     }
   }
