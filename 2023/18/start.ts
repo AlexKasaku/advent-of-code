8628499fcedab33ead440c372427b596b8ee3a00
Alex Washtell <alex@kasaku.co.uk>
2023-12-18 09:53:22 +0000

    2023 - Day 18 - Some tidy up

--- 2023/18/start.ts
+++ 2023/18/start.ts
@@ -17,8 +17,7 @@ const debugMode = false;
 const debug = (...params: any[]) => debugMode && console.log(...params);
 const log = (...params: any[]) => console.log(...params);
 
-const part1 = (steps: Step[]): void => {
-  // Part 1, still using a grid:
+const calculateWithGrid = (steps: Step[]): void => {
   const points = getPointsFromSteps(steps);
   const [topLeft, bottomRight] = calculateBounds(points);
 
@@ -27,22 +26,26 @@ const part1 = (steps: Step[]): void => {
   const startingPosition = { x: Math.abs(topLeft.x), y: Math.abs(topLeft.y) };
 
   digGrid(grid, startingPosition, steps);
-  //renderGrid(grid);
   fillFromCenter(grid);
-  //renderGrid(grid);
+
+  renderGrid(grid);
 
   const dugPoints = grid.Values.flat()
     .map((s) => (s.dug ? 1 : 0))
     .reduce(toSum, 0);
-  log(dugPoints);
+
+  log(`Total: ${dugPoints}`);
 };
 
-const part2 = (steps: Step[]): void => {
+const fastCalculate = (steps: Step[]): void => {
   const points = getPointsFromSteps(steps);
 
-  log(points);
+  debug(points);
 
-  // Calculate area within polygon
+  // Calculate area within polygon.
+  // Reference: https://www.mathopenref.com/coordpolygonarea.html
+  // Reference: https://www.mathopenref.com/coordpolygonarea2.html
+  // This works because the area is simple with no intersections
   let area = 0;
   for (let index = 0; index < points.length; index++) {
     const product1 = points[index].x * points[(index + 1) % points.length].y;
@@ -52,9 +55,10 @@ const part2 = (steps: Step[]): void => {
   }
   area = Math.abs(area) / 2;
 
-  log(`Area: ${area}`);
+  debug(`Area: ${area}`);
 
-  // Add on perimeter
+  // Add on 1/2 the perimeter. The way we've got the points means we need to account for an additional
+  // layer in the X + Y axis. This could perhaps be just accounted for by amending the points?
   let perimeter = 0;
   for (let index = 0; index < points.length - 1; index++) {
     const a = points[index];
@@ -63,15 +67,22 @@ const part2 = (steps: Step[]): void => {
   }
   const perimeterArea = perimeter / 2 + 1;
 
-  log(`Perimeter: ${perimeterArea}`);
+  debug(`Perimeter: ${perimeterArea}`);
   log(`Total: ${area + perimeterArea}`);
 };
 
 const start = async (file: string) => {
   const content = fs.readFileSync(path.join(__dirname, file), 'utf8');
 
+  // Part1 = true Part2 = false
   const steps = parseInput(content, false);
-  part2(steps);
+
+  // Method used for part 1. Very slow but allows for rendering.
+  //calculateWithGrid(steps);
+
+  // Method used for part 2 (and works for part 1). Just calculates the area quickly, works
+  // for extremely large polygons.
+  fastCalculate(steps);
 };
 
 start('./files/example.txt');
